408679b4228fa540e1a4969a7d9a4b37022ad1d0
Levin Faber <LFA@timetoact.group>
2018-10-30 15:16:39 +0100

    :building_construction: Starting React Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Test</h1>
+
+      // TODO Control
+      // TODO Game
+        // TODO Players
+        // TODO Chat
+        // TODO Round
+          //TODO Yourcards
+    );
+  }
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+/*
+const href = location.href;
+let roomID = "";
+if (href.indexOf('#') >= 0) {
+    roomID = href.slice(href.lastIndexOf('/') + 1);
+    document.querySelector('input[name="RName"]').value = roomID;
+} 
+const players = [];
+let roundNr = 0;
+const UUIDGeneratorBrowser = () =>
+    ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
+    );
+const myUUID = UUIDGeneratorBrowser();
+*/
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
