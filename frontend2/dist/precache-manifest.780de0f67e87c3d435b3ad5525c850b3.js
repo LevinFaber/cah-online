e7166395a183ce77fc2fb479c960eec782ac8b41
Levin <itsdumbledumb@gmail.com>
2018-11-04 19:38:09 +0100

    Dist

---
+++ frontend2/dist/precache-manifest.780de0f67e87c3d435b3ad5525c850b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "1f94b463c4cbbc6bc552",
+    "url": "/js/chunk-vendors.4985f2bc.js"
+  },
+  {
+    "revision": "3fc0967b53cd00285497",
+    "url": "/js/app.9cb9032e.js"
+  },
+  {
+    "revision": "7202894e318d772a186d471d168b1583",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f94b463c4cbbc6bc552",
+    "url": "/css/chunk-vendors.3eccd4a2.css"
+  },
+  {
+    "revision": "3fc0967b53cd00285497",
+    "url": "/css/app.997a83e5.css"
+  }
+];
